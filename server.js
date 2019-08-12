e327fb824ddea41b6255f1303a7249ced6926581
Uwise Mohamed <peakyblinder@Uwises-MacBook-Pro.local>
2019-08-11 23:58:14 -0700

    Set up express server

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app=express();
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the link-repo API'}));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
