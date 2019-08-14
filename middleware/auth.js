96c35715ac7a9d20456757e8e432db8743b10a39
Uwise Mohamed <peakyblinder@Uwises-MacBook-Pro.local>
2019-08-13 18:40:48 -0700

    Set up middleware for protected routes

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config= require('config');
+
+module.exports = function(req, res, next){
+
+    //Get Token from the header
+    const token = req.header('x-auth-token');
+
+    //Check if not token
+    if(!token){
+        return res.status(401).json({msg: 'No Token found. Authorization Denied'});
+
+    }
+    try{
+        const decoded= jwt.verify(token, config.get('jwtSecret'));
+        req.user=decoded.user;
+        next();
+    }
+    catch(err){
+        res.status(401).json({msg: 'Invalid Token. Authorization Denied'});
+
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+
+const auth=require('../middleware/auth');
 const {check, validationResult} = require('express-validator');
 const User = require('../models/User');
 
@@ -11,8 +13,15 @@ const User = require('../models/User');
 //@desc    Get the loggedin user
 //@access  Private
 
-router.get('/', (req,res) => {
-    res.send('Get Logged-in user');
+router.get('/', auth, async (req,res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).json('Server Error');
+    }
 });
 
 //@route   POST api/auth
