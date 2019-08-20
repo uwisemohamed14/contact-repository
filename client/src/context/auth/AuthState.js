5b84f4bdfad323041466dc2ceafa2afccd8e4dc4
Uwise Mohamed <peakyblinder@Uwises-MacBook-Pro.local>
2019-08-20 13:51:04 -0700

    Login with token implemented

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -54,6 +54,7 @@ const AuthState = props => {
             dispatch({
                 type: REGISTER_SUCCESS, payload: res.data
             })
+            loadUser();
         }
         catch(err){
             dispatch({
@@ -64,8 +65,28 @@ const AuthState = props => {
     }
 
     //Login User
-    const login = () => console.log('load');
-
+    const login = async formData => {
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+        try{
+            const res = await axios.post('/api/auth', formData, config);
+            dispatch({
+                type: LOGIN_SUCCESS, 
+                payload: res.data
+            })
+            loadUser();
+        }
+        catch(err){
+            dispatch({
+                type: LOGIN_FAIL, 
+                payload: err.response.data.msg
+            });
+        }
+        
+    };
 
     //Logout 
     const logout = () => console.log('load');
