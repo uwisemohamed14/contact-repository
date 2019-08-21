534485a7b0c96bc8788aafd05cdca69cd5aa8d2f
Uwise Mohamed <peakyblinder@Uwises-MacBook-Pro.local>
2019-08-21 15:52:39 -0700

    Production Ready(Overwrite)

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const app=express();
+const path = require('path');
 
-//Connect DB
+const app = express();
+
+// Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the link-repo API'}));
-
-//Initialize Middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-//Routes
-
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/links', require('./routes/links'));
+app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-const PORT = process.env.PORT || 5000
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
